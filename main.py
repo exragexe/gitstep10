1be13f4260d9177a129f2b4ad9dd06cebf19ad8d
yaros <yaroslavdisiak@gmail.com>
2022-11-22 22:22:28 +0200

    ex3done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+
+
+try:
+
+    n = int(input("Enter num: "))
+
+    res = {1: lambda n: all(n % i != 0 for i in range(2, int(n**.5)+1))}
+    print(res[1](n))
+
+
+
+except Exception as e:
+    print(e)
+
