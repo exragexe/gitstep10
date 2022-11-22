a9d716e60fa3ed4432dffb5bfec4a15c15535913
yaros <yaroslavdisiak@gmail.com>
2022-11-22 20:14:51 +0200

    ex2done

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 
+
+try:
+    import random
+
+    size = int(input(" Enter amount numbers: "))
+    begin = -100
+    end = 100
+    my_text = []
+
+    for l in range(size):
+        my_text.append(random.randint(begin, end))
+
+    random.shuffle(my_text)
+    print(my_text)
+    ###########################
+    num = {1: lambda ls: ls[::-1]}
+    print(num[1](my_text))
+
+except Exception as e:
+    print(e)
